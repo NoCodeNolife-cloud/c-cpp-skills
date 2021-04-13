e9a03ff5accff642156605ab14a3b727ce41eb48
蔡伟 <1607575130@qq.com>
2021-04-13 13:35:45 +0800

    update

---
+++ map and pair/map and pair.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+
+int main ( int argc , char* argv [] ) {
+	std::pair<int , std::string>ppair ( 245 , "yeziyu" );/*pair*/
+	std::fstream fout ( "out.txt" , std::ios::out );
+	fout << "ppair.first = " << ppair.first << std::endl << "ppair.second = " << ppair.second << std::endl;
+	std::map<int , std::string>mmap;/*map*/
+	mmap [ 2 ] = "liangjiawen";
+	mmap [ 4 ] = "yangjingtao";
+	fout << "map is \n";
+	for ( std::pair<int , std::string> x : mmap ) {
+		fout << x.first << " : " << x.second << std::endl;
+	}
+	return 0;
+}
