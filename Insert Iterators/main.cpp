76fffefa9271ca5a63dfe7c29ec87198eacd928c
蔡伟 <1607575130@qq.com>
2021-04-15 22:48:45 +0800

    update

---
+++ Insert Iterators/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <deque>
+#include <set>
+#include <algorithm>
+
+int main () {
+	std::list<int> coll1;
+
+	// insert elements from 1 to 9 into the first collection
+	for ( int i = 1; i <= 9; ++i ) {
+		coll1.push_back ( i );
+	}
+
+	// copy the elements of coll1 into coll2 by appending them
+	std::vector<int> coll2;
+	std::copy ( coll1.begin () , coll1.end () , std::back_inserter ( coll2 ) );
+
+	// copy the elements of coll1 into coll2 by appending them
+	std::deque<int>coll3;
+	std::copy ( coll1.begin () , coll1.end () , std::front_inserter ( coll3 ) );
+
+	// copy elements of coll1 into coll4
+	std::set<int>coll4;
+	std::copy ( coll1.begin () , coll1.end () , std::inserter ( coll4 , coll4.begin () ) );
+
+	// exit
+	std::exit ( EXIT_SUCCESS );
+}
---
+++ stream Iterators/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+int main () {
+	std::vector<std::string> coll;
+	std::copy ( std::istream_iterator<std::string> ( std::cin ) , std::istream_iterator<std::string> () , std::back_inserter ( coll ) );// read all words from the standard input
+	sort ( coll.begin () , coll.end () );// sort elements
+	std::unique_copy ( coll.begin () , coll.end () , std::ostream_iterator<std::string> ( std::cout , "\n" ) );// print all elements without duplicates
+	system ( "pause" );// check for the result
+	std::exit ( EXIT_SUCCESS );// exit
+}
