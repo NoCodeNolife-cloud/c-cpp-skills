0d6827e69eed7693d8a4350647070c93981f6150
蔡伟 <1607575130@qq.com>
2021-04-17 11:58:34 +0800

    update

---
+++ Binary Pridicates/main.cpp
@@ -0,0 +1,56 @@
+﻿// head
+#include <iostream>
+#include <string>
+#include <deque>
+#include <fstream>
+#include <iomanip>
+#include <algorithm>
+
+// object
+class Point {
+public:
+	int x;
+	int y;
+};
+
+// compare
+bool cmp ( const Point& p1 , const Point& p2 ) {
+	if ( p1.x > p2.x ) {
+		return true;
+	} else if ( p1.x == p2.x and p1.y > p2.y ) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+int main () {
+	// open file
+	std::fstream fout ( "out.txt" , std::ios::out | std::ios::app );
+
+	// point collection
+	std::deque<Point> coll ( 50 );
+
+	// random
+	srand ( time ( NULL ) );
+
+	// initialize the point
+	for ( Point& item : coll ) {
+		item.x = rand () % 100;
+		item.y = rand () % 100;
+	}
+
+	// sort Point
+	std::sort ( coll.begin () , coll.end () , cmp );
+
+	// print Point
+	for ( Point item : coll ) {
+		fout << std::setw ( 5 ) << std::left << item.x << item.y << std::endl;
+	}
+
+	// close the file
+	fout.close ();
+
+	// exit
+	std::exit ( EXIT_SUCCESS );
+}
---
+++ unary predicates/main.cpp
@@ -0,0 +1,52 @@
+﻿// head
+#include <iostream>
+#include <list>
+#include <algorithm>
+#include <fstream>
+#include <cstdlib>
+
+// predicate, which return whether an integer is a prime number
+bool isPrime ( int number ) {
+	// ignore negative sign
+	number = abs ( number );
+
+	// 0 and 1 are prime numbers
+	if ( number == 0 or number == 1 ) {
+		return true;
+	}
+
+	// find divisor that divides without a remainder
+	int divisor;
+	for ( divisor = number / 2; number % divisor != 0; --divisor ) {
+	}
+
+	// if no divisor greater than 1 is find it is a prime number
+	return divisor == 1;
+}
+
+int main () {
+	// file
+	std::fstream fout ( "out.txt" , std::ios::out | std::ios::app );
+
+	// list
+	std::list<int> coll;
+
+	// insert element from 24 to 30
+	for ( int i = 24; i <= 30; ++i ) {
+		coll.push_back ( i );
+	}
+
+	// search for prime number
+	std::list<int>::iterator pos = std::find_if ( coll.begin () , coll.end () , isPrime );
+	if ( pos != coll.end () ) {
+		//found
+		fout << *pos << " is first prime number fount" << std::endl;
+	} else {
+		//not found
+		fout << "no prime number found" << std::endl;
+	}
+
+	// exit
+	system ( "PAUSE" );
+	std::exit ( EXIT_SUCCESS );
+}
