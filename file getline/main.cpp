77ef92f339e808db238b42f8aecd3dc86d7198f0
蔡伟 <1607575130@qq.com>
2021-04-18 15:50:46 +0800

    update

---
+++ file getline/main.cpp
@@ -0,0 +1,24 @@
+﻿// head
+#include <iostream>
+#include <string>
+#include <fstream>
+
+// main
+int main () {
+	// open file
+	std::fstream fin ( "in.txt" , std::ios::in );
+	std::fstream fout ( "out.txt" , std::ios::out );
+
+	while ( !fin.eof () ) {
+		std::string line;
+		std::getline ( fin , line );
+		fout << line << std::endl;
+	}
+
+	// close file
+	fin.close ();
+	fout.close ();
+
+	// exit
+	std::exit ( EXIT_SUCCESS );
+}
