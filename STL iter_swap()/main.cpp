0ae7254db77cf1562446bf9e73cdccab56dbd0c2
蔡伟 <1607575130@qq.com>
2021-04-23 23:59:08 +0800

    update

---
+++ STL iter_swap()/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <list>
+#include <fstream>
+#include <algorithm>
+
+namespace cod {
+	// print
+	template<typename T>
+	void print ( std::list<T> _list );
+}
+
+int main () {
+	std::list<int>coll;// assertion list
+
+	// insert elements from 1 to 9
+	for ( int i = 1; i <= 9; ++i ) {
+		coll.push_back ( i );
+	}
+
+	cod::print ( coll );// print value
+	std::iter_swap ( coll.begin () , ++coll.begin () );// swap first and second value
+	cod::print ( coll );// print value
+	std::iter_swap ( coll.begin () , --coll.end () );// swap first and last value
+	cod::print ( coll );// print value
+	return 0;// exit
+}
+
+template<typename T>
+void cod::print ( std::list<T> _list ) {
+	std::fstream fout ( "out.txt" , std::ios::app );// open file
+
+	// print
+	copy ( _list.begin () , _list.end () , std::ostream_iterator<int> ( fout , " " ) );
+	fout << std::endl;
+
+	fout.close ();// close file
+	return;
+}
---
+++ STL reverse iterator/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <fstream>
+#include <list>
+#include <algorithm>
+
+namespace cod {
+	// print element
+	template<typename T>
+	void  print ( T elem );
+}
+
+int main () {
+	std::fstream fout ( "out.txt" , std::ios::app );// open file
+	std::list<int> coll;// assertion list
+
+	// insert element from 1 to 9
+	for ( int i = 1; i <= 9; ++i ) {
+		coll.push_back ( i );
+	}
+
+	// print all elements in normal order
+	std::for_each ( coll.begin () , coll.end () , cod::print<int> );
+	fout << std::endl;
+
+	// print all elements in reverse order
+	std::for_each ( coll.rbegin () , coll.rend () , cod::print<int> );
+	fout << std::endl;
+
+	fout.close ();// close file
+	return 0;// exit
+}
+
+template<typename T>
+void cod::print ( T elem ) {
+	std::fstream fout ( "out.txt" , std::ios::app );// open file
+	fout << elem << " ";// print
+	fout.close ();// close file
+	return;
+}
