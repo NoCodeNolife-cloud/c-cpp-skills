fab9207a55465ac274eb1d49f8222083fb1c8aa5
蔡伟 <1607575130@qq.com>
2021-04-24 11:53:26 +0800

    update

---
+++ STL iterator base()/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <list>
+#include <algorithm>
+#include <fstream>
+
+int main () {
+	std::fstream fout ( "out.txt" , std::ios::app );// open file
+	std::list<int>coll;// assertion list
+
+	// insert elements from 1 to 9
+	for ( int i = 1; i <= 9; ++i ) {
+		coll.push_back ( i );
+	}
+
+	std::list<int>::iterator pos = std::find ( coll.begin () , coll.end () , 5 );// find position of elements with value 5
+	fout << "*pos: " << *pos << std::endl;// print value of the element
+	std::list<int>::reverse_iterator rpos ( pos );// convert iterator to reverse iterator
+	fout << "rpos: " << *rpos << std::endl;// print value of the element to which the reverse iterator refers
+	std::list<int>::iterator rrpos = rpos.base ();// convert reverse iterator back to normal iterator
+	fout << "rrpos: " << *rrpos << std::endl;
+
+	fout.close ();// close file
+	return 0;// exit
+}
---
+++ STL reverse_iterator/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <deque>
+#include <algorithm>
+#include <fstream>
+
+namespace cod {
+	// print number
+	template<typename T>
+	void print ( T elem ) {
+		std::fstream fout ( "out.txt" , std::ios::app );// open file
+		fout << elem << " ";// print
+		fout.close ();// close file
+		return;
+	}
+}
+
+int main () {
+	std::fstream fout ( "out.txt" , std::ios::app );// open file
+	std::deque<int> coll;// deque
+
+	// insert elements from 1 to 9
+	for ( int i = 1; i <= 9; ++i ) {
+		coll.push_back ( i );
+	}
+
+	std::deque<int>::iterator pos1 = std::find ( coll.begin () , coll.end () , 2 );// find 2 position
+	std::deque<int>::iterator pos2 = std::find ( coll.begin () , coll.end () , 7 );// find 7 position
+	std::for_each ( pos1 , pos2 , cod::print<int> );// print
+	fout << std::endl;
+
+	// convert iterator to reverse iterator
+	std::deque<int>::reverse_iterator rpos1 ( pos1 );
+	std::deque<int>::reverse_iterator rpos2 ( pos2 );
+
+	std::for_each ( rpos2 , rpos1 , cod::print<int> );// print
+	fout << std::endl;
+	fout.close ();// close file
+	return 0;// exit
+}
