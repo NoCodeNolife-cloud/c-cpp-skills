fd00260482d584b4b7a6ea11a7744a1f67cf7ffb
蔡伟 <1607575130@qq.com>
2021-04-26 16:57:36 +0800

    update

---
+++ display time/display time/display time.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main () {
+	time_t now = time ( 0 );// 基于当前系统的当前日期/时间
+	char* dt = ctime ( &now );// 把 now 转换为字符串形式
+	cout << "本地日期和时间：" << dt << endl;
+	system ( "pause" );
+}
