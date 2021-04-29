62998a0a35564ebf049ff31bab3f3fbbdec7e40a
蔡伟 <1607575130@qq.com>
2021-04-29 17:22:04 +0800

    update

---
+++ Custom function adapter/Custom function adapter/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <functional>
+#include <fstream>
+#include <algorithm>
+
+template<class T1 , class T2>
+struct fopow :public std::binary_function<T1 , T2 , T1> {
+	T1 operator()( T1 base , T2 exp )const {
+		return std::pow ( base , exp );
+	}
+};
+
+int main () {
+	std::fstream fout ( "out.txt" , std::ios::app );
+	std::vector<int>coll;
+
+	// insert
+	for ( int i = 1; i <= 9; ++i ) {
+		coll.push_back ( i );
+	}
+
+	std::transform ( coll.begin () , coll.end () , std::ostream_iterator<int> ( fout , " " ) , std::bind1st ( fopow<float , int> () , 3 ) );
+	fout << std::endl;
+
+	std::transform ( coll.begin () , coll.end () , std::ostream_iterator<int> ( fout , " " ) , std::bind2nd ( fopow<float , int> () , 3 ) );
+	fout << std::endl;
+
+	fout.close ();
+}
---
+++ STL bind fonctor/STL bind fonctor/STL bind fonctor.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+#include <functional>
+
+int main () {
+	std::fstream fout ( "out.txt" , std::ios::app );
+
+	int a [] = { 1, 2, 100, 200 };
+
+	std::vector< int> arr ( a , a + 4 );
+
+	// 移除所有小于100的元素
+	arr.erase ( std::remove_if ( arr.begin () , arr.end () , std::bind2nd ( std::less<int> () , 100 ) ) , arr.end () );
+
+	for ( int item : arr ) {
+		fout << item << " ";
+	}
+	fout << std::endl;
+
+	std::vector< int> arr2 ( a , a + 4 );
+
+	// 移除所有大于100的元素
+	arr2.erase ( std::remove_if ( arr2.begin () , arr2.end () , std::bind1st ( std::less<int> () , 100 ) ) , arr2.end () );
+
+	for ( int item : arr2 ) {
+		fout << item << " ";
+	}
+	fout << std::endl;
+
+	fout.close ();
+}
