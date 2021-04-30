5fc2f4226709dcbd665623b17f9d87bfab7cf922
蔡伟 <1607575130@qq.com>
2021-04-30 16:17:54 +0800

    update

---
+++ compose_f_gx/compose_f_gx/compose_f_gx.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include "Compose_f_gx_t.h"
+
+void PRINT_ELEMENTS ( std::vector<int> _coll ) {
+	for ( std::vector<int>::iterator pos = _coll.begin (); pos != _coll.end (); ++pos ) {
+		std::cout << *pos << " ";
+	}
+	std::cout << std::endl;
+}
+
+int main () {
+	std::vector<int>coll;
+
+	// insert elements from 1 to 9
+	for ( int i = 1; i <= 9; ++i ) {
+		coll.push_back ( i );
+	}
+
+	PRINT_ELEMENTS ( coll );
+
+	// and 10 then multiply 5
+	std::transform ( coll.begin () , coll.end () , std::ostream_iterator<int> ( std::cout , " " ) , compose_f_gx ( std::bind2nd ( std::multiplies<int> () , 5 ) , std::bind2nd ( std::plus<int> () , 10 ) ) );
+	std::cout << std::endl;
+
+	system ( "PAUSE" );
+}
---
+++ compose_f_gx_hx/compose_f_gx_hx/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include "compose_f_gx_hx_t.h"
+
+template<typename T>
+void PRINT_ELEMENTS ( T& _coll ) {
+	for ( auto pos = _coll.begin (); pos != _coll.end (); ++pos ) {
+		std::cout << *pos << " ";
+	}
+	std::cout << std::endl;
+}
+
+int main () {
+	std::vector<int>coll;
+
+	for ( int i = 1; i <= 9; ++i ) {
+		coll.push_back ( i );
+	}
+
+	PRINT_ELEMENTS ( coll );
+
+	// 判断某值是否大于4且小于7
+	coll.erase ( std::remove_if ( coll.begin () , coll.end () , compose_f_gx_hx ( std::logical_and<bool> () , std::bind2nd ( std::greater<int> () , 4 ) , std::bind2nd ( std::less<int> () , 7 ) ) ) , coll.end () );
+
+	PRINT_ELEMENTS ( coll );
+
+	system ( "PAUSE" );
+}
