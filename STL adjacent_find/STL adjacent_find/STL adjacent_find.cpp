fc6d8f3d9db20baaca0bfa57f5ee52090855b0df
蔡伟 <1607575130@qq.com>
2021-05-06 23:57:03 +0800

    update

---
+++ STL adjacent_find/STL adjacent_find/STL adjacent_find.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template<class T>
+inline void PRINT_ELEMENTS ( const T& coll , const char* optcstr = "" ) {
+	typename T::const_iterator pos;
+	std::cout << optcstr;
+	for ( pos = coll.begin (); pos != coll.end (); ++pos ) {
+		std::cout << *pos << " ";
+	}
+	std::cout << std::endl;
+}
+
+template<class T>
+inline void INSERT_ELEMENTS ( T& coll , int first , int last ) {
+	for ( int i = first; i <= last; ++i ) {
+		coll.insert ( coll.end () , i );
+	}
+}
+
+bool doubled ( int elem1 , int elem2 ) {
+	return elem1 * 2 == elem2;
+}
+
+int main () {
+	std::vector<int>coll;
+
+	coll.push_back ( 1 );
+	coll.push_back ( 3 );
+	coll.push_back ( 2 );
+	coll.push_back ( 4 );
+	coll.push_back ( 5 );
+	coll.push_back ( 5 );
+	coll.push_back ( 0 );
+
+	PRINT_ELEMENTS ( coll , "coll: " );
+
+	std::vector<int>::iterator pos = std::adjacent_find ( coll.begin () , coll.end () );
+	if ( pos != coll.end () ) {
+		std::cout << "first two elements with equal value have position "
+			<< std::distance ( coll.begin () , pos ) + 1
+			<< std::endl;
+	}
+
+	pos = std::adjacent_find ( coll.begin () , coll.end () , doubled );
+	if ( pos != coll.end () ) {
+		std::cout << "first two elements with second value twice the first have pos. "
+			<< std::distance ( coll.begin () , pos ) + 1
+			<< std::endl;
+	}
+
+	system ( "pause" );
+}
---
+++ STL equal/STL equal/STL equal.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+template<class T>
+inline void PRINT_ELEMENTS ( const T& coll , const char* optcstr = "" ) {
+	typename T::const_iterator pos;
+	std::cout << optcstr;
+	for ( pos = coll.begin (); pos != coll.end (); ++pos ) {
+		std::cout << *pos << " ";
+	}
+	std::cout << std::endl;
+}
+
+template<class T>
+inline void INSERT_ELEMENTS ( T& coll , int first , int last ) {
+	for ( int i = first; i <= last; ++i ) {
+		coll.insert ( coll.end () , i );
+	}
+}
+
+bool bothEvenOrOdd ( int elem1 , int elem2 ) {
+	return elem1 % 2 == elem2 % 2;
+}
+
+int main () {
+	std::vector<int>coll1;
+	std::list<int>coll2;
+
+	INSERT_ELEMENTS ( coll1 , 1 , 7 );
+	INSERT_ELEMENTS ( coll2 , 3 , 9 );
+
+	PRINT_ELEMENTS ( coll1 , "coll1: " );
+	PRINT_ELEMENTS ( coll2 , "coll2: " );
+
+	if ( std::equal ( coll1.begin () , coll1.end () , coll2.begin () ) ) {
+		std::cout << "coll1 == coll2" << std::endl;
+	} else {
+		std::cout << "coll1 != coll2" << std::endl;
+	}
+
+	if ( std::equal ( coll1.begin () , coll1.end () , coll2.begin () , bothEvenOrOdd ) ) {
+		std::cout << "even and odd elemets correspond" << std::endl;
+	} else {
+		std::cout << "even and odd elemets do not correspond" << std::endl;
+	}
+	system ( "pause" );
+}
