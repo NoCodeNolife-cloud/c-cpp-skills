cb20312391b4ac732bac157d4a4a0e190dab095c
蔡伟 <1607575130@qq.com>
2021-05-09 09:42:20 +0800

    update

---
+++ STL fill and fill_n/STL fill and fill_n/STL fill and fill_n.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <list>
+
+template<class T>
+inline void PRINT_ELEMENTS ( const T& coll , const char* optcstr = "" ) {
+	typename T::const_iterator pos;
+	std::cout << optcstr;
+	for ( pos = coll.begin (); pos != coll.end (); ++pos ) {
+		std::cout << *pos << " ";
+	}
+	std::cout << std::endl;
+}
+
+template<class T>
+inline void INSERT_ELEMENTS ( T& coll , int first , int last ) {
+	for ( int i = first; i <= last; ++i ) {
+		coll.insert ( coll.end () , i );
+	}
+}
+
+int main () {
+	std::fill_n ( std::ostream_iterator<int> ( std::cout , " " ) , 10 , 7.7 );
+	std::cout << std::endl;
+
+	std::list<std::string>coll;
+
+	std::fill_n ( std::back_inserter ( coll ) , 9 , "hello" );
+	PRINT_ELEMENTS ( coll , "coll: " );
+
+	std::fill ( coll.begin () , coll.end () , "again" );
+	PRINT_ELEMENTS ( coll , "coll: " );
+
+	std::fill_n ( coll.begin () , coll.size () - 2 , "hi" );
+	PRINT_ELEMENTS ( coll , "coll: " );
+
+	std::list<std::string>::iterator pos1 = coll.begin ();
+	std::list<std::string>::iterator pos2 = coll.end ();
+
+	std::fill ( ++pos1 , --pos2 , "hmmm" );
+	PRINT_ELEMENTS ( coll , "coll: " );
+
+	system ( "pause" );
+}
---
+++ STL transform combine/STL transform combine/main.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <vector>
+#include <list>
+
+template<class T>
+inline void PRINT_ELEMENTS ( const T& coll , const char* optcstr = "" ) {
+	std::fstream fout ( "out.txt" , std::ios::app );
+	typename T::const_iterator pos;
+	fout << optcstr;
+	for ( pos = coll.begin (); pos != coll.end (); ++pos ) {
+		fout << *pos << " ";
+	}
+	fout << std::endl;
+	fout.close ();
+}
+
+template<class T>
+inline void INSERT_ELEMENTS ( T& coll , int first , int last ) {
+	for ( int i = first; i <= last; ++i ) {
+		coll.insert ( coll.end () , i );
+	}
+}
+
+int main () {
+	std::fstream fout ( "out.txt" , std::ios::app );
+	std::vector<int> coll1;
+	std::list<int>coll2;
+
+	INSERT_ELEMENTS ( coll1 , 1 , 9 );
+	PRINT_ELEMENTS ( coll1 , "coll1: " );
+
+	std::transform ( coll1.begin () , coll1.end () , coll1.begin () , coll1.begin () , std::multiplies<int> () );
+	PRINT_ELEMENTS ( coll1 , "squared: " );
+
+	std::transform ( coll1.begin () , coll1.end () , coll1.rbegin () , std::back_inserter ( coll2 ) , std::plus<int> () );
+	PRINT_ELEMENTS ( coll2 , "coll2: " );
+
+	fout << "diff: ";
+	std::transform ( coll1.begin () , coll1.end () , coll2.begin () , std::ostream_iterator<int> ( fout , " " ) , std::minus<int> () );
+	fout << std::endl;
+
+	fout.close ();
+	system ( "pause" );
+}
