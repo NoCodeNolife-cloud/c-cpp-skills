2c0d216e5b21ecd859fde1e962fd88cd9f35eb6a
蔡伟 <1607575130@qq.com>
2021-05-10 23:23:13 +0800

    create

---
+++ STL reverse and reverse_copy/STL reverse and reverse_copy/STL reverse and reverse_copy.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <functional>
+#include <algorithm>
+
+template<class T>
+inline void PRINT_ELEMENTS ( const T& coll , const char* optcstr = "" ) {
+	typename T::const_iterator pos;
+	std::cout << optcstr;
+	for ( pos = coll.begin (); pos != coll.end (); ++pos ) {
+		std::cout << *pos << " ";
+	}
+	std::cout << std::endl;
+}
+
+template<class T>
+inline void INSERT_ELEMENTS ( T& coll , int first , int last ) {
+	for ( int i = first; i <= last; ++i ) {
+		coll.insert ( coll.end () , i );
+	}
+}
+
+int main () {
+	std::vector<int>coll;
+
+	INSERT_ELEMENTS ( coll , 1 , 9 );
+	PRINT_ELEMENTS ( coll , "coll: " );
+
+	std::reverse ( coll.begin () , coll.end () );
+	PRINT_ELEMENTS ( coll , "coll: " );
+
+	std::reverse ( coll.begin () + 1 , coll.end () - 1 );
+	PRINT_ELEMENTS ( coll , "coll: " );
+
+	std::reverse_copy ( coll.begin () , coll.end () , std::ostream_iterator<int> ( std::cout , " " ) );
+	std::cout << std::endl;
+
+	system ( "pause" );
+}
---
+++ STL unique_copy/STL unique_copy/STL unique_copy.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <list>
+#include <functional>
+
+template<class T>
+inline void PRINT_ELEMENTS ( const T& coll , const char* optcstr = "" ) {
+	typename T::const_iterator pos;
+	std::cout << optcstr;
+	for ( pos = coll.begin (); pos != coll.end (); ++pos ) {
+		std::cout << *pos << " ";
+	}
+	std::cout << std::endl;
+}
+
+template<class T>
+inline void INSERT_ELEMENTS ( T& coll , int first , int last ) {
+	for ( int i = first; i <= last; ++i ) {
+		coll.insert ( coll.end () , i );
+	}
+}
+
+bool differenceOne ( int elem1 , int elem2 ) {
+	return elem1 + 1 == elem2 || elem1 - 1 == elem2;
+}
+
+int main () {
+	int source [] = { 1,4,4,6,1,2,2,3,1,6,6,6,5,7,5,4,4 };
+	int sourceNum = sizeof ( source ) / sizeof ( source [ 0 ] );
+
+	std::list<int>coll;
+	copy ( source , source + sourceNum , std::back_inserter ( coll ) );
+	PRINT_ELEMENTS ( coll );
+
+	std::unique_copy ( coll.begin () , coll.end () , std::ostream_iterator<int> ( std::cout , " " ) );
+	std::cout << std::endl;
+
+	std::unique_copy ( coll.begin () , coll.end () , std::ostream_iterator<int> ( std::cout , " " ) , differenceOne );
+	std::cout << std::endl;
+
+	system ( "pause" );
+}
