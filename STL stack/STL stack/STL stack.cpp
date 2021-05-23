5ae8defe696225bceb20055750d683e3aa68b707
蔡伟 <1607575130@qq.com>
2021-05-23 22:15:58 +0800

    update

---
+++ STL stack/STL stack/STL stack.cpp
@@ -0,0 +1,29 @@
+﻿#include <stack>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	stack<int> stk;
+
+	cout << "size = " << stk.size() << endl;
+
+	stk.push(6);
+	cout << "size = " << stk.size() << endl;
+
+	stk.push(5);
+	cout << "size = " << stk.size() << endl;
+
+	stk.push(4);
+	cout << "size = " << stk.size() << endl;
+
+	stk.pop();
+	cout << stk.top() << endl;
+
+	stk.top() = 10;
+	cout << stk.top() << endl;
+
+	cout << "size = " << stk.size() << endl;
+}
