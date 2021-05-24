a92127444c8ee5d89964b406ad01f7307f34c89e
蔡伟 <1607575130@qq.com>
2021-05-24 14:04:40 +0800

    update

---
+++ STL priority_queue/STL priority_queue/STL priority_queue.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int main() {
+
+	priority_queue<int> que;
+	if (que.empty()) {
+
+		for (int i = 5; i >= 0; --i) {
+
+			que.push(i);
+		}
+	}
+
+	cout << "front = " << que.top() << endl;
+}
---
+++ STL queue/STL queue/STL queue.cpp
@@ -0,0 +1,29 @@
+﻿#include <queue>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	queue<int>que;
+
+	cout << "initial size = " << que.size() << endl;
+
+	que.push(5);
+	cout << "after add, size = " << que.size() << endl;
+
+	que.push(6);
+	cout << "after add, size = " << que.size() << endl;
+
+	que.push(7);
+	cout << "after add, size = " << que.size() << endl;
+
+	cout << "front = " << que.front() << endl;
+	cout << "back = " << que.back() << endl;
+
+	que.pop();
+	cout << "after pop, size = " << que.size() << endl;
+	cout << "front = " << que.front() << endl;
+	cout << "back = " << que.back() << endl;
+}
