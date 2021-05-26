ee34e28903ad53ef9b9eefffc6b623011bacbc12
蔡伟 <1607575130@qq.com>
2021-05-26 23:19:56 +0800

    update

---
+++ string compare/cmake-build-debug/CMakeFiles/string_compare.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/string_compare.dir/main.cpp.obj"
+  "libstring_compare.dll.a"
+  "string_compare.exe"
+  "string_compare.exe.manifest"
+  "string_compare.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/string_compare.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ string compare/string compare/string compare.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	string str;
+	cout << "str:";
+	cin >> str;
+
+	string target = "loupe";
+
+	if (str > target) {
+
+		cout << str << ">" << target << endl;
+	}
+	else {
+
+		cout << str << "<" << target << endl;
+	}
+}
