26862a1116393dbd6154464cc92434101893a7d8
蔡伟 <1607575130@qq.com>
2021-06-16 20:20:38 +0800

    update

---
+++ Use stream buffer directly/cmake-build-debug/CMakeFiles/Use_stream_buffer_directly.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Use_stream_buffer_directly.dir/main.cpp.obj"
+  "Use_stream_buffer_directly.exe"
+  "Use_stream_buffer_directly.exe.manifest"
+  "Use_stream_buffer_directly.pdb"
+  "libUse_stream_buffer_directly.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Use_stream_buffer_directly.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
