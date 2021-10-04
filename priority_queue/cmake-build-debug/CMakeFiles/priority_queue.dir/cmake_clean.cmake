36e56ed260f04b621f59f9d46888dc34bedea546
落奈 <54143278+NoCodeNolife-cloud@users.noreply.github.com>
2021-10-04 21:13:06 +0800

    update

---
+++ priority_queue/cmake-build-debug/CMakeFiles/priority_queue.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/priority_queue.dir/main.cpp.obj"
+  "libpriority_queue.dll.a"
+  "priority_queue.exe"
+  "priority_queue.exe.manifest"
+  "priority_queue.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/priority_queue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
