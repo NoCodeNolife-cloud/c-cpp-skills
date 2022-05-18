6ac8b2c21060d40f8d482079e3bfc251611ae20a
NoCodeNolife-cloud <54143278+NoCodeNolife-cloud@users.noreply.github.com>
2022-05-18 21:48:15 +0800

    update

---
+++ shuffle/cmake-build-debug/CMakeFiles/shuffle.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shuffle.dir/main.cpp.obj"
+  "libshuffle.dll.a"
+  "shuffle.exe"
+  "shuffle.exe.manifest"
+  "shuffle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shuffle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
