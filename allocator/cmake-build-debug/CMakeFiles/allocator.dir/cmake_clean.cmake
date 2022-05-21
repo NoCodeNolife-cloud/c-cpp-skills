e314c60d7a365ec502a17d86d0cc9ed5d36303c1
NoCodeNolife-cloud <54143278+NoCodeNolife-cloud@users.noreply.github.com>
2022-05-22 00:09:26 +0800

    update

---
+++ allocator/cmake-build-debug/CMakeFiles/allocator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/allocator.dir/main.cpp.obj"
+  "allocator.exe"
+  "allocator.exe.manifest"
+  "allocator.pdb"
+  "liballocator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/allocator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
