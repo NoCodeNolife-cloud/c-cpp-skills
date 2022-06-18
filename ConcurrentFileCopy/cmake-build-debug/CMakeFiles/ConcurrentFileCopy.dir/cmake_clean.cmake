b79b1b732d8395234a2d7db36585c4cccfc6ac67
NoCodeNolife-cloud <54143278+NoCodeNolife-cloud@users.noreply.github.com>
2022-06-18 23:31:47 +0800

    update

---
+++ ConcurrentFileCopy/cmake-build-debug/CMakeFiles/ConcurrentFileCopy.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ConcurrentFileCopy.dir/ConcurrentFileCopy.cpp.obj"
+  "ConcurrentFileCopy.exe"
+  "ConcurrentFileCopy.exe.manifest"
+  "ConcurrentFileCopy.pdb"
+  "libConcurrentFileCopy.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ConcurrentFileCopy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
